e731de7dca7122922e5c856c36fa87c200024d61
reddevilcero <reddevil_cero@hotmail.com>
2020-01-28 11:28:46 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,16 @@ class Person
   @@all = []
 
   def initialize
+    say_hi
    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def say_hi
+    puts "hello creator. you are my good."
   end
 end
 
